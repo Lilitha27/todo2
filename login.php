75f787bed94c05d7cae388b2b89d07b18569c968
Lilitha27 <lilithakganya@gmail.com>
2019-10-10 12:02:32 +0200

    updated my login file

--- login.php
+++ login.php
@@ -35,25 +35,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password FROM users WHERE username = :username";
         
-        if($stmt = $mysqli->prepare($sql)){
+        if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("s", $param_username);
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             
             // Set parameters
-            $param_username = $username;
+            $param_username = trim($_POST["username"]);
             
             // Attempt to execute the prepared statement
             if($stmt->execute()){
-                // Store result
-                $stmt->store_result();
-                
                 // Check if username exists, if yes then verify password
-                if($stmt->num_rows == 1){                    
-                    // Bind result variables
-                    $stmt->bind_result($id, $username, $hashed_password);
-                    if($stmt->fetch()){
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["id"];
+                        $username = $row["username"];
+                        $hashed_password = $row["password"];
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
                             session_start();
@@ -80,11 +78,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
         
         // Close statement
-        $stmt->close();
+        unset($stmt);
     }
     
     // Close connection
-    $mysqli->close();
+    unset($pdo);
 }
 ?>
  
@@ -93,12 +91,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <!-- <link rel="styelsheet" href="css/style.css" type="text/css"> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    
-    <link href="CSS/styles.css" type="text/css" rel="stylesheet">
+    <!-- <link rel="styelsheet" href="css/style.css" type="text/css"> -->
+     
 </head>
 <body>
-    <!-- <div class="wrapper">
+    <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
@@ -113,38 +113,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+                <input type="submit" class="btn btn-primary" id="button" value="Login">
             </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+            <p>Don't have an account? <a href="register.php" id="sign"> Sign up now</a>.</p>
         </form>
-    </div>     -->
-
-
-<form class="login-form" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-  <p class="login-text">
-    <span class="fa-stack fa-lg">
-      <i class="fa fa-circle fa-stack-2x"></i>
-      <i class="fa fa-lock fa-stack-1x"></i>
-    </span>
-  </p>
-  <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-  <input type="text" name="username" class="login-username" autofocus="true" required="true" placeholder="Username" value="<?php echo $username; ?>" />
-  <span class="help-block"><?php echo $username_err; ?></span>
-</div>
-
-<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-  <input type="password" name="password" class="login-password" required="true" placeholder="Password" />
-  <span class="help-block"><?php echo $password_err; ?></span>
-</div> 
-
-  <input type="submit" name="submit" value="Login" class="login-submit" />
-
-
-<a href="register.php" class="login-forgot-pass">Sign Up Now</a>
-<div class="underlay-photo"></div>
-<div class="underlay-black"></div> 
-</form>
+    </div>
 </body>
-
-
 </html>
